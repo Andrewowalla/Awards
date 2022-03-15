a51bb32ac7fd379bf34c53279c5ec4cf8df6de29
Andrewowalla <andrewowalla@gmail.com>
2022-03-15 08:52:45 +0300

    Feature: Project model class

--- awward/models.py
+++ awward/models.py
@@ -4,8 +4,16 @@ from cloudinary.models import CloudinaryField
 
 
 # Create your models here.
-class User(AbstractUser):
+class Profile(models.Model):
     name = models.CharField(max_length=200, null=True)
     email = models.EmailField(null=True, unique=True)
     bio = models.TextField(null=True)
     profile_pic = CloudinaryField('images')
+
+class Project(models.Model):
+    name = models.CharField(max_length=100)
+    date_added = models.DateTimeField(auto_now_add=True)
+    description = models.CharField(max_length=250)
+    repo_link = models.CharField(max_length=500)
+    live_link = models.CharField(max_length=500, blank=True)
+    image = CloudinaryField('images', null=True)
