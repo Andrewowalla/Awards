e9b9ef137710f344dc7df0ba705e90b61f1ff7b6
Andrewowalla <andrewowalla@gmail.com>
2022-03-15 08:57:32 +0300

    Feature: Create Model class for Rate that will be used for rating the projects

--- awward/models.py
+++ awward/models.py
@@ -1,6 +1,7 @@
 from django.db import models
 from django.contrib.auth.models import AbstractUser
 from cloudinary.models import CloudinaryField
+from django.core.validators import MinValueValidator, MaxValueValidator
 
 
 # Create your models here.
@@ -20,3 +21,11 @@ class Project(models.Model):
 
     def __str__(self):
         return self.name
+
+class Rate(models.Model):
+    project = models.ForeignKey(Project, on_delete=models.CASCADE)
+    design_vote = models.IntegerField(default=0, validators=[MaxValueValidator(10), MinValueValidator(0)])
+    usability_vote = models.IntegerField(default=0, validators=[MaxValueValidator(10), MinValueValidator(0)])
+    content_vote = models.IntegerField(default=0, validators=[MaxValueValidator(10), MinValueValidator(0)])
+    comment = models.TextField()
+    created = models.DateTimeField(auto_now_add=True, null=True)
