68aa07d2a8d0f2f1001862eb8d7524d5bdca8c6d
Andrewowalla <andrewowalla@gmail.com>
2022-03-15 09:15:16 +0300

    Settings: Add special route to display uploaded images and update the template context processor setting

--- awwards/settings.py
+++ awwards/settings.py
@@ -130,6 +130,9 @@ STATICFILES_DIRS = [
     os.path.join(BASE_DIR, "static"),
 ]
 
+MEDIA_URL = '/media/'
+MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
+
 # adding config
 cloudinary.config( 
   cloud_name = "do7r1eyot", 
